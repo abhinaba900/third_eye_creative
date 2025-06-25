b55d63f33c66b4df5a464e9c9a19bd72c3c1fa9e
Abhinaba Jana <abhinabajana900@gmail.com>
2025-06-25 16:41:00 +0530

    font size increaseed

--- src/app/components/OurExpertise.tsx
+++ src/app/components/OurExpertise.tsx
@@ -27,7 +27,7 @@ function OurExpertise() {
           backgroundColor="transparent"
           wireframes={false}
           gravity={0.56}
-          fontSize="1.5rem"
+          fontSize="1.8rem"
           setIsHoveredForAnimation={setIsHoveredForAnimation}
           mouseConstraintStiffness={0.9}
         />
