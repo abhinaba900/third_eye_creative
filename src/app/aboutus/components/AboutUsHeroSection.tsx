63f9ee6ff419f6715ed3483008283542c29141f4
Abhinaba Jana <abhinabajana900@gmail.com>
2025-07-09 16:32:45 +0530

    task fixed in large screen

--- src/app/aboutus/components/AboutUsHeroSection.tsx
+++ src/app/aboutus/components/AboutUsHeroSection.tsx
@@ -12,9 +12,7 @@ function AboutUsHeroSection() {
         height={"100"}
         className="mx-auto my-4 background-image-hero-section-about-us"
       /> */}
-      <div
-        className="about-us-hero-section-wrapper overflow-hidden"
-      >
+      <div className="about-us-hero-section-wrapper overflow-hidden">
         <h3 className="about-us-hero-section-title">THIRD EYE</h3>
         <div className="relative w-full ">
           <Aurora
@@ -23,15 +21,17 @@ function AboutUsHeroSection() {
             amplitude={1.0}
             speed={1}
           />
-          <div className=" about-us-hero-section-content-wrapper">
-            <h4>
-              Designing Bold Identities & Intelligent <span>Experiences</span>{" "}
-              for Brands Ready to Lead & Inspire
-            </h4>
-            <div className="about-us-hero-section-footer-wrapper">
-              <p>©2025</p>
-              <p>BASED IN B’LORE</p>
-              <p>AGENCY</p>
+          <div className="about-us-hero-section-content-wrapper-inner">
+            <div className=" about-us-hero-section-content-wrapper">
+              <h4>
+                Designing Bold Identities & Intelligent <span>Experiences</span>{" "}
+                for Brands Ready to Lead & Inspire
+              </h4>
+              <div className="about-us-hero-section-footer-wrapper">
+                <p>©2025</p>
+                <p>BASED IN B’LORE</p>
+                <p>AGENCY</p>
+              </div>
             </div>
           </div>
         </div>
