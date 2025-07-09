df7664fad88084ad5066f20cb6df863272033839
Abhinaba Jana <abhinabajana900@gmail.com>
2025-07-09 16:42:11 +0530

    mobile view fixed

--- src/app/work/components/WorkHeroSection.tsx
+++ src/app/work/components/WorkHeroSection.tsx
@@ -13,7 +13,12 @@ function WorkHeroSection() {
         className="mx-auto my-4 background-image-hero-section-about-us"
       /> */}
       <div className="about-us-hero-section-wrapper ">
-        <h3 className="about-us-hero-section-title " style={{fontSize: "19vw"}}>OUR WORK</h3>
+        <h3
+          className="about-us-hero-section-title "
+          style={{ fontSize: "19vw" }}
+        >
+          OUR WORK
+        </h3>
         <div className="relative">
           <Aurora
             colorStops={["#C883FF", "#772BF2", "#CE0AFF00"]}
@@ -21,14 +26,17 @@ function WorkHeroSection() {
             amplitude={1.0}
             speed={1}
           />
-          <div className="relative about-us-hero-section-content-wrapper">
-            <h4>
-              Pixels with Purpose, Code with Character, Results that <span>Resonate</span>
-            </h4>
-            <div className="about-us-hero-section-footer-wrapper">
-              <p>PIXEL PERFECT</p>
-              <p>CASE FULL OF WOW</p>
-              <p>WORK @ THIRD EYE</p>
+          <div className="about-us-hero-section-content-wrapper-inner">
+            <div className="relative about-us-hero-section-content-wrapper">
+              <h4>
+                Pixels with Purpose, Code with Character, Results that{" "}
+                <span>Resonate</span>
+              </h4>
+              <div className="about-us-hero-section-footer-wrapper">
+                <p>PIXEL PERFECT</p>
+                <p>CASE FULL OF WOW</p>
+                <p>WORK @ THIRD EYE</p>
+              </div>
             </div>
           </div>
         </div>
