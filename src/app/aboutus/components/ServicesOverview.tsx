71e634adeaf980b4a04d8f91bab99cca608cdb76
Abhinaba Jana <abhinabajana900@gmail.com>
2025-10-17 16:46:25 +0530

    image overview fixed

--- src/app/aboutus/components/ServicesOverview.tsx
+++ src/app/aboutus/components/ServicesOverview.tsx
@@ -1,7 +1,7 @@
 ﻿"use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/navigation";
 
