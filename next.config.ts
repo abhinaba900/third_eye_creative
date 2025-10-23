b3071690a795b7646aa44346f7db5bfc6512c8e0
Abhinaba Jana <abhinabajana900@gmail.com>
2025-10-23 17:17:52 +0530

    fixed the sectin and updated

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [new URL("https://images.unsplash.com"), new URL("https://picsum.photos")],
     unoptimized: true,
   },
-  output: "export",
 };
 
 export default nextConfig;
